faeb8eee26f66c057a89877276e38f457629b97e
Yvens Rabelo <yvens@yvensrabelo.com>
2025-09-16 19:46:50 -0400

    Corrigir configuração do Vercel e adicionar debug

--- app.js
+++ app.js
@@ -18,9 +18,15 @@ const client = new MercadoPagoConfig({
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+
+// Debug middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
 
 // Rota principal
 app.get('/', (req, res) => {
